94f5e436ddd884669389e96668ec8e14e97787bd
Jerome Noveda <noveda.jerome@gmail.com>
2015-02-24 18:00:49 +0800

    check if session is active for the rest of the pages

--- controller/upload.php
+++ controller/upload.php
@@ -4,10 +4,17 @@ new Upload();
 
 class Upload{
 	public function __construct(){
-		$this->index();
+		session_start();
+
+		if($_SESSION['id'] != null){
+			$this->index();
+		}else{
+			echo 'wahahaha';
+		}
 	}
 
 	function index(){
+		$session = $_SESSION['id'];
 		include '../view/header.php';
 		include '../view/pages/upload.php';
 		include '../view/footer.php';
--- controller/view.php
+++ controller/view.php
@@ -6,13 +6,19 @@ new View();
 class View{
 	private $model_files;
 	public function __construct(){
+		session_start();
 		$this->model_files = new model_files();
 
-		$this->index();
+		if($_SESSION['id'] != null){
+			$this->index();
+		}else{
+			echo 'wahahaha';
+		}
 
 	}
 
 	function index(){
+		$session = $_SESSION['id'];
 		$files = $this->model_files->view_submitted_files();
 		include '../view/header.php';
 		include '../view/pages/view.php';
