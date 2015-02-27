79d5f1c07ae367ce826f4c282de1ab1099d2fadd
Jerome Noveda <noveda.jerome@gmail.com>
2015-02-27 14:44:05 +0800

    fixed markup bug

--- view/pages/view.php
+++ view/pages/view.php
@@ -68,18 +68,19 @@
 
       				      <div id="f-description" class="form-group">
       				        <label for="f-desc" class="col-sm-2 control-label">Description</label>
-      				        <div class="col-sm-10">
-      				          <textarea name="description" class="form-control" ></textarea>
-      				        </div>
-      				      </div>
-      				<div class="modal-footer">
-      					<div class="form-group">
-    						<div class="col-sm-offset-2 col-sm-10">
-      							<button type="submit" class="btn btn-default" data-loading-text="Uploading ...." autocomplete="off">Upload</button>
-    						</div>
-  						</div>
-      				</div>
-    			</div>
-  			</div>
-  		</form>
-		</div>
+	   				        <div class="col-sm-10">
+   				          <textarea name="description" class="form-control" ></textarea>
+   				        </div>
+			      </div>
+   				</div>
+   			<div class="modal-footer">
+   				<div class="form-group">
+   					<div class="col-sm-offset-2 col-sm-10">
+   						<button type="submit" class="btn btn-default" data-loading-text="Uploading ...." autocomplete="off">Upload</button>
+   					</div>
+				</div>
+   			</div>
+   			</div>
+   			</div>
+  	</form>
+</div>
